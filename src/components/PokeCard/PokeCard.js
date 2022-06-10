e31e3e16561f7e273f1c1580c82bc5a1bb645bcc
Lucas <luan237@gmail.com>
2022-06-09 22:53:18 -0400

    delete unnecessary files

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -1,7 +1,7 @@
+import axios from "axios";
+
 function PokeCard() {
-    return (
-        <h1>Hello</h1>
-    )
+  return <h1>Hello</h1>;
 }
 
-export default PokeCard
+export default PokeCard;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,22 +1,20 @@
 import React from "react";
-import "./HomePage.scss"
-import versus from "../../assets/vs.png"
+import "./HomePage.scss";
+import versus from "../../assets/vs.png";
 import PokeCard from "../../components/PokeCard/PokeCard";
 
 class HomePage extends React.Component {
-
-
-    render() {
-        return (
-            <>  
-                <main className="main">
-                    <PokeCard/>
-                    <img src={versus}></img>
-                    <PokeCard/>
-                </main>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <main className="main">
+          <PokeCard />
+          <img src={versus} alt="versus"></img>
+          <PokeCard />
+        </main>
+      </>
+    );
+  }
 }
 
 export default HomePage;
