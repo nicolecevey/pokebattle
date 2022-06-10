ef45ef5b879b4f2c4c5904a5241dc11eb8972236
Nicole Cevey <nicole.cevey@gmail.com>
2022-06-10 09:55:34 -0400

    Adding endpoint to get api data

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,9 +2,22 @@ import React from "react";
 import "./HomePage.scss"
 import versus from "../../assets/vs.png"
 import PokeCard from "../../components/PokeCard/PokeCard";
+import axios from "axios";
+
+const pokemonEndpoint = "http://localhost:8080/pokemon";
 
 class HomePage extends React.Component {
 
+    componentDidMount(){
+        document.title = "PokeBattle";
+
+        axios
+            .get(pokemonEndpoint)
+            .then((response) => {
+                console.log(response.data);
+            })
+
+    }
 
     render() {
         return (
