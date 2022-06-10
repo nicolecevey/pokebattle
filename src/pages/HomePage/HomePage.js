b598da5e56f9834808be5f6efa682053a6bdd6ab
lucas-bui <luan237@gmail.com>
2022-06-10 11:12:36 -0400

    styling halfway

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./HomePage.scss"
-import versus from "../../assets/vs.png"
+import "./HomePage.scss";
+import versus from "../../assets/vs.png";
 import PokeCardOne from "../../components/PokeCardOne/PokeCardOne";
 import PokeCardTwo from "../../components/PokeCardTwo/PokeCardTwo";
 import axios from "axios";
@@ -8,49 +8,55 @@ import axios from "axios";
 const pokemonEndpoint = "http://localhost:8080/pokemon";
 
 class HomePage extends React.Component {
+  state = {
+    pokemonOne: null,
+    pokemonTwo: null,
+  };
 
-    state = {
-        pokemonOne: null,
-        pokemonTwo: null,
-    }
+  componentDidMount() {
+    document.title = "PokeBattle";
+    this.fetchRandomPokemon();
+  }
 
-    componentDidMount(){
-        document.title = "PokeBattle";
+  fetchRandomPokemon = async () => {
+    const pokemon1 = await axios
+      .get(`${pokemonEndpoint}/random`)
+      .then((response) => {
+        return response.data;
+      });
+    const pokemon2 = await axios
+      .get(`${pokemonEndpoint}/random`)
+      .then((response) => {
+        return response.data;
+      });
+    this.setState({
+      pokemonOne: pokemon1,
+      pokemonTwo: pokemon2,
+    });
+  };
+  //   fetchPokemons = () => {
+  //     return axios.get(pokemonEndpoint);
+  //   };
 
-        axios
-            .get(`${pokemonEndpoint}/random`)
-            .then((response) => {
-                this.setState({
-                    pokemonOne: response.data,
-                    pokemonTwo: response.data
-                })
-            })
-    }
-
-    fetchPokemons = () => {
-        return axios.get(pokemonEndpoint);
-    }
-
-    render() {
-        return (
-            <>  
-                <main className="main">
-                    <PokeCardOne 
-                        poke1={this.state.pokemonOne}/>
-                    <div className="attack">
-                        <img 
-                            src={versus}
-                            className="vs-icon"
-                            >
-                        </img>
-                        <button>Attack</button>
-                    </div>
-                    <PokeCardTwo 
-                        poke2={this.state.pokemonTwo}/>
-                </main>
-            </>
-        )
-    }
+  render() {
+    this.state.pokemonOne && console.log(this.state.pokemonOne);
+    return (
+      <>
+        <main className="main">
+          {this.state.pokemonOne && (
+            <PokeCardOne poke1={this.state.pokemonOne} />
+          )}
+          <div className="attack">
+            <img src={versus} className="vs-icon"></img>
+            <button>Attack</button>
+          </div>
+          {this.state.pokemonTwo && (
+            <PokeCardTwo poke2={this.state.pokemonTwo} />
+          )}
+        </main>
+      </>
+    );
+  }
 }
 
 export default HomePage;
